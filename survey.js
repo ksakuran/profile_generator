e014539b4e13b51fe5ae9b82e13553a844d8c655
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-18 03:51:39 +0000

    a command line survey app that generates a descriptive profile based on the answers recieved

---
+++ survey.js
@@ -0,0 +1,46 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("'What's your name? Nicknames are also acceptable :) :", (answer1) => {
+  console.log(`Thank you for your valuable feedback: ${answer1}`);rl.question("What's an activity you like doing? ", (answer2) => {
+    console.log(`Thank you for your valuable feedback: ${answer2}`);
+
+    rl.question("What do you listen to while doing that? ", (answer3) => {
+      console.log(`Thank you for your valuable feedback: ${answer3}`);
+
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (answer4) => {
+        console.log(`Thank you for your valuable feedback: ${answer4}`);
+
+        rl.question("What's your favourite thing to eat for that meal? ", (answer5) => {
+          console.log(`Thank you for your valuable feedback: ${answer5}`);
+
+          rl.question("Which sport is your absolute favourite? ", (answer6) => {
+            console.log(`Thank you for your valuable feedback: ${answer6}`);
+
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer7) => {
+              console.log(`Thank you for your valuable feedback: ${answer7}`);
+              console.log("---");
+              console.log("Here is your new profile!");
+              console.log("  ");
+              process.stdout.write(`${answer1} loves listening to ${answer3} while ${answer2}, \ndevouring ${answer5} for ${answer4}, \nprefers ${answer6} over any other sport,\nand is amazing at ${answer7}.`)
+              console.log("\n");
+              rl.close();
+            });
+            
+          });
+          
+        });
+        
+      });
+      
+    });
+      
+  });
+  
+});
+
+
